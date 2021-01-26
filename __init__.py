c25f1d0facc435a022a5148f34c51489ecf49768
gabaaa <not-set>
2021-01-26 13:25:30 -0600

    adding base files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# authors: Gabriel Auger
+# name: etconf
+# licenses: MIT 
+__version__ = "0.0.0"
+
+# from .dev.etconf import etconf
+# from .gpkgs import message as msg
+# from .gpkgs.options import Options
