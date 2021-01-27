1d8e428cc1b1422231a373cbce491add935a6404
gabaaa <not-set>
2021-01-26 22:35:27 -0600

    minor change

--- __init__.py
+++ __init__.py
@@ -2,8 +2,6 @@
 # authors: Gabriel Auger
 # name: etconf
 # licenses: MIT 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from .dev.etconf import Etconf
-# from .gpkgs import message as msg
-# from .gpkgs.options import Options
